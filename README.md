acbeb851b9ea495797fbabf6b40df78834279cb0
Iris Koester <ikoester@eonerc.rwth-aachen.de>
2019-06-06 15:14:04 +0200

    added setup.py

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # FiLiP
 
 Python library for easing the handling of the FiwareAPI from python
+
+Installation:
+'''
+python setup.py install
+'''
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='filip',
+    version='1.0',
+    description='[FI]WARE [li]brary in [p]ython',
+    author='RWTH Aachen University',
+    author_email='tstorek@eonerc.rwth-aachen.de',
+    packages=['filip'],
+    install_requires=['requests']
+)
