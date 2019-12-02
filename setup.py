2dbb133cf24ee373fcf7492dffc750f14e9e6c21
thomas.storek <thomas.storek@rwth-aachen.de>
2019-12-02 14:03:59 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
-from setuptools import setup
+import setuptools
 
-setup(
+INSTALL_REQUIRES = ['requests',
+                    'fuzzywuzzy',
+                    'PyYAML']
+
+SETUP_REQUIRES = INSTALL_REQUIRES.copy()
+
+
+setuptools.setup(
     name='filip',
     version='1.0',
     description='[FI]WARE [li]brary in [p]ython',
-    author='RWTH Aachen University',
+    author='RWTH Aachen University, E.ON Energy Research Center, Institute\
+                     of Energy Efficient Buildings and Indoor Climate',
+    # Specify the Python versions you support here. In particular, ensure
+    # that you indicate whether you support Python 2, Python 3 or both.
+    classifiers=['Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7', ],
     author_email='tstorek@eonerc.rwth-aachen.de',
-    packages=['filip'],
-    install_requires=['requests', 'fuzzywuzzy', 'fuzzywuzzy[speedup]']
+    packages=setuptools.find_packages(exclude=['img']),
+    setup_requires=SETUP_REQUIRES,
+    install_requires=INSTALL_REQUIRES,
 )
