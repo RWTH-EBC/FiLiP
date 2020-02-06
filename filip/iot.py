52614c295212cecfe13784892a088eb8a2da5364
Felix Rehmann <Felix.Rehmann@rwth-aachen.de>
2020-02-06 18:25:24 +0100

    added functionality to add attributes via json

--- filip/iot.py
+++ filip/iot.py
@@ -130,6 +130,41 @@ class Device:
             print("[WARN]: Attribute type unknown: \"{}\"".format(attr_type))
 
 
+    def add_attribute_json(self, attribute:dict):
+        """
+        :param attribute: {
+            "name": "Temp_Sensor",
+            "value_type": "Number",
+            "attr_type": "Static",
+            "attr_value": "12"}
+
+
+        :param name: The name of the attribute as submitted to the context broker.
+        :param type: The type of the attribute as submitted to the context broker.
+        :param object_id: The id of the attribute used from the southbound API.
+        :param attr_type: One of \"active\" (default), \"lazy\" or \"static\"
+        """
+
+
+        attr_type = attribute["attr_type"]
+        if "attr_value" in attribute:
+            if attribute["attr_type"] != "static":# & attribute["attr_value"] != None:
+                print('[WARNING] Setting attribute value only allowed for \
+                        static attributes! Value will be ignored!')
+                del attribute["attr_value"]
+
+        attr = {"name": attribute["name"],
+                "type": attribute["value_type"],
+                "object_id": attribute["object_id"]}
+        # attr["value"] = Attribute.value NOT Supported by agent-lib
+        switch_dict = {"active": self.add_active,
+                "lazy": self.add_lazy,
+                "static":  self.add_static,
+                "command": self.add_command
+                }.get(attr_type, "not_ok")(attr)
+        if switch_dict == "not_ok":
+            print("[WARN]: Attribute type unknown: \"{}\"".format(attr_type))
+
 
     def delete_attribute(self, attr_name, attr_type):
         '''
@@ -350,6 +385,7 @@ class DeviceGroup:
         dict['commands'] = self.__commands
         dict['static_attributes'] = self.__static_attributes
         dict['internal_attributes'] = self.__internal_attributes
+        print(json.dumps(dict, indent=4))
         return json.dumps(dict, indent=4)
 
     def generate_apikey(self, length: int = 10):
@@ -464,6 +500,7 @@ class Agent:
         if response.status_code not in [201, 200, 204]:
             print("[WARN]: Unable to update device group:\n")
             print(response.text)
+            print(payload)
             print("payload")
         else:
             print("[INFO]: Device group successfully updated!")
@@ -475,6 +512,7 @@ class Agent:
         payload={}
         payload['devices'] = [json.loads(device.get_json())]
         payload = json.dumps(payload, indent=4)
+        print(payload, 'this is the payload')
         response = requests.request("POST", url, data=payload,
                                     headers=headers)
         if response.status_code != 201:
