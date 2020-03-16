004079f54c362b30b1474db7353b73b29a4aecbc
Steffen Vogel <post@steffenvogel.de>
2020-03-16 13:43:51 +0100

    add Python 3.8 to list of supported versions

--- setup.py
+++ setup.py
@@ -20,7 +20,8 @@ setuptools.setup(
     # that you indicate whether you support Python 2, Python 3 or both.
     classifiers=['Programming Language :: Python :: 3.5',
                  'Programming Language :: Python :: 3.6',
-                 'Programming Language :: Python :: 3.7', ],
+                 'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.8'],
     author_email='tstorek@eonerc.rwth-aachen.de',
     packages=setuptools.find_packages(exclude=['img']),
     setup_requires=SETUP_REQUIRES,
