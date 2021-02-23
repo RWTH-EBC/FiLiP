1e45e726b87b09d859fd12e1e341f56c389fd2f7
Thomas Storek <tstorek@eonerc.rwth-aachen.de>
2021-02-23 09:58:22 +0100

    added BaseClient
    For #45

---
+++ filip/core/base_client.py
@@ -0,0 +1,36 @@
+import logging
+import requests
+from core.models import FiwareHeader
+
+logger = logging.getLogger(__name__)
+
+
+class _BaseClient:
+    """
+    This class implements an base client for all derived api-clients.
+    """
+
+    def __init__(self, session: requests.Session = None,
+                 fiware_header: FiwareHeader = None):
+        """
+
+        Args:
+            session:
+            fiware_header:
+        """
+        # TODO: Double Check Header Handling
+        self.session = session or requests.Session()
+        self.headers = dict()
+        if not fiware_header:
+            fiware_header = FiwareHeader()
+        self.headers.update(fiware_header.dict(by_alias=True))
+
+    # Context Manager Protocol
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def close(self):
+        self.session.close()
