da42838c25d87d15639ab7c7338585391fa9e953
Thomas Storek <tstorek@eonerc.rwth-aachen.de>
2021-05-25 16:42:07 +0200

    added missing tests for configuration and sessions
    For #17 #25

--- tests/test_core_client.py
+++ tests/test_core_client.py
@@ -3,52 +3,121 @@ Test for filip.core.client
 """
 import unittest
 import requests
+import json
 from pathlib import Path
 from filip.core.models import FiwareHeader
 from filip.core.client import Client
+
 
 class TestClient(unittest.TestCase):
     """
     Test case for global client
     """
+
     def setUp(self) -> None:
         """
         Setup test data
         Returns:
             None
         """
-        self.fh = FiwareHeader(service='n5geh',
-                               service_path='/#')
+        self.fh = FiwareHeader(service='filip',
+                               service_path='/testing')
+        self.config = json.loads("test_core_client.json")
 
-        self.config = {'cb_url': 'http://134.130.166.184:1026',
-                       'iota_url': 'http://134.130.166.184:4041',
-                       'ql_url': 'http://134.130.166.184:8668'}
+    def _test_change_of_headers(self, client: Client):
+        """
+        Test changes in fiware headers
+        Args:
+            client (Client): Client under test
+        Returns:
+            None
+        """
+        client.fiware_service = 'filip_other'
+        self.assertEqual(client.fiware_service_path,
+                         client.cb.fiware_service_path,
+                         'FIWARE service out of sync')
+        self.assertEqual(client.fiware_service_path,
+                         client.iota.fiware_service_path,
+                         'FIWARE service out of sync')
+        self.assertEqual(client.fiware_service_path,
+                         client.timeseries.fiware_service_path,
+                         'FIWARE service out of sync')
+
+        client.fiware_service_path = '/someOther'
+        self.assertEqual(client.fiware_service_path,
+                         client.cb.fiware_service_path,
+                         'FIWARE Service path out of sync')
+        self.assertEqual(client.fiware_service_path,
+                         client.iota.fiware_service_path,
+                         'FIWARE Service path out of sync')
+        self.assertEqual(client.fiware_service_path,
+                         client.timeseries.fiware_service_path,
+                         'FIWARE Service path out of sync')
+
+    def _test_connections(self, client: Client):
+        """
+        Test connections of sub clients
+        Args:
+            client (Client): Client under test
+        Returns:
+            None
+        """
+        client.cb.get_version()
+        client.iota.get_version()
+        client.timeseries.get_version()
 
     def test_config_dict(self):
-        with requests.Session() as s:
-            client=Client(config=self.config, session=s, fiware_header=self.fh)
-            client.fiware_service_path = '/someOther'
-            print(client.cb.get_version())
-            print(client.cb.get_entity_list())
-            print(client.cb.session)
+        client = Client(config=self.config, fiware_header=self.fh)
+        self._test_connections(client=client)
+        self._test_change_of_headers(client=client)
 
     def test_config_json(self):
-        config_path = Path("./test_core_client.json")
+        """
+        Test with configuration with json-file
+        Returns:
+
+        """
+        config_path = Path("test_core_client.json")
         client = Client(config=config_path, fiware_header=self.fh)
-        print(client.cb.get_version())
-        print(client.cb.get_resources())
-        print(client.cb.get_entity_list())
+        self._test_connections(client=client)
+        self._test_change_of_headers(client=client)
 
     def test_config_env(self):
+        """
+        Test configuration using .env.filip or environment variables the
+        latter is handled in core.config
+        Returns:
+            None
+        """
         client = Client(fiware_header=self.fh)
-        print(client.cb.get_version())
-        print(client.iota.get_version())
-        print(client.timeseries.get_version())
+        self._test_connections(client=client)
+        self._test_change_of_headers(client=client)
+
+    def test_session_handling(self):
+        """
+        Test session handling for performance boost and credential handling
+        Returns:
+            None
+        """
+        # with new session object
+        with Client(config=self.config,
+                    fiware_header=self.fh,
+                    reuse_session=True) as client:
+            self._test_connections(client=client)
+            self._test_change_of_headers(client=client)
+
+        # with outer session
+        with requests.Session() as s:
+            client = Client(config=self.config,
+                            session=s,
+                            fiware_header=self.fh)
+            self._test_connections(client=client)
+            self._test_change_of_headers(client=client)
 
     def tearDown(self) -> None:
         """
-
+        Clean up artifacts
         Returns:
-            pass
+            None
         """
         pass
