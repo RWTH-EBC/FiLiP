cbf0512395e064014542f265d2827963cf34653b
Thomas Storek <tstorek@eonerc.rwth-aachen.de>
2021-08-04 16:10:36 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, RWTH Aachen University, ' \
 author = 'EON EBC'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.1'
 
 # The short X.Y version.
 version = '0.1'
