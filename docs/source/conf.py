a60e88ccae5f5b02d246d7492993fb4e48638100
tzu-chen.liu <tzu-chen.liu@rwth-aachen.de>
2021-08-13 12:26:10 +0200

    raise version number for #12

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, RWTH Aachen University, ' \
 author = 'EON EBC'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.4'
+release = '0.1.5'
 
 # The short X.Y version.
 version = '0.1'
