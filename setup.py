590fe8a843227714a16801b8025e06f4d0ed3bb8
Thomas Storek <tstorek@eonerc.rwth-aachen.de>
2021-10-04 11:13:04 +0200

    removed deprecated dependencies
    For #32

--- setup.py
+++ setup.py
@@ -15,9 +15,7 @@ INSTALL_REQUIRES = ['aenum',
                     'datamodel-code-generator>=0.10.2',
                     'pandas-datapackage-reader>=0.18.0',
                     'pydantic[dotenv]>=1.7.2',
-                    'tables',
-                    'requests_oauthlib',
-                    'authlib']
+                    'tables']
 
 SETUP_REQUIRES = INSTALL_REQUIRES.copy()
 
