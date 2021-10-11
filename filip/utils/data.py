d9ba38c8daa9433fdf07ab2dde81fc806a283236
Thomas Storek <tstorek@eonerc.rwth-aachen.de>
2021-10-11 18:05:16 +0200

    implemented pickle manager https://stackoverflow.com/questions/63329657/python-3-7-error-unsupported-pickle-protocol-5
    For #38

--- filip/utils/data.py
+++ filip/utils/data.py
@@ -1,12 +1,52 @@
 import os
+import importlib
 import logging
 from pathlib import Path
+import pickle
 from typing import Dict
 import pandas as pd
 from pandas_datapackage_reader import read_datapackage
 from filip.utils.validators import validate_http_url
 
 logger = logging.getLogger(__name__)
+
+
+class PickleProtocolManager:
+    """
+    Context Manager for pickle protocol
+    https://stackoverflow.com/questions/60067953/is-it-possible-to-specify-the-pickle-protocol-when-writing-pandas-to-hdf5
+    """
+    def __init__(self, level: int):
+        """
+        Initilize with protocol level
+        Args:
+            level: level of pickle portocol
+        """
+        self.previous = pickle.HIGHEST_PROTOCOL
+        assert level <= self.previous, f"According to your python version " \
+                                       f"the max protocol level is: " \
+                                       f"{self.previous}"
+        self.level = level
+
+    def __enter__(self):
+        importlib.reload(pickle)
+        pickle.HIGHEST_PROTOCOL = self.level
+
+    def __exit__(self, *exc):
+        importlib.reload(pickle)
+        pickle.HIGHEST_PROTOCOL = self.previous
+
+
+def pickle_protocol(level: int) -> PickleProtocolManager:
+    """
+    Caller for pickle protocol handler
+    Args:
+        level: int
+
+    Returns:
+        Protocol Handler
+    """
+    return PickleProtocolManager(level)
 
 
 def load_datapackage(url: str, filename: str) -> Dict[str, pd.DataFrame]:
@@ -29,7 +69,6 @@ def load_datapackage(url: str, filename: str) -> Dict[str, pd.DataFrame]:
     path.mkdir(parents=True, exist_ok=True)
     filepath = path.joinpath(filename)
 
-
     if os.path.isfile(filepath):
         # read data from filip.data if exists
         logger.info("Found existing data package in 'filip.data'")
@@ -48,7 +87,8 @@ def load_datapackage(url: str, filename: str) -> Dict[str, pd.DataFrame]:
             # store data in filip.data
             for k, v in data.items():
                 v.loc[:, :] = v[:].applymap(str)
-                v.to_hdf(str(filepath), key=k)
+                with pickle_protocol(4):
+                    v.to_hdf(str(filepath), key=k)
         except:
             logger.error("Failed to load data package!")
             raise
