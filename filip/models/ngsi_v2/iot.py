f9f4e4b3a487400728e344827a94aa52812c55bc
Reding Jeff <jeff.reding@rwth-aachen.de>
2021-10-18 14:43:58 +0200

    Fixed Issue 58, removed the overriding of the new metadata validator

--- filip/models/ngsi_v2/iot.py
+++ filip/models/ngsi_v2/iot.py
@@ -151,19 +151,6 @@ class DeviceAttribute(BaseAttribute):
         description="name of the attribute as coming from the device."
     )
 
-    @validator('metadata')
-    def validate_metadata(cls, data):
-        """
-        Check metadata object for serialization and units if present.
-        Args:
-            value: value of metadata field
-        Returns:
-
-        """
-        assert json.dumps(data), "metadata not serializable"
-        for k, v in data.items():
-            data.update(NamedContextMetadata(name=k, **v).to_context_metadata())
-        return data
 
 class LazyDeviceAttribute(DeviceAttribute):
     """
