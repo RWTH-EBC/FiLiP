901160ebe8d3ff54e9d51df47141efd2a7327dae
FelixStege <felix.stegemerten@rwth-aachen.de>
2022-01-18 17:53:31 +0100

    chore: fix typos

--- tutorial/e3_thermal_zone.py
+++ tutorial/e3_thermal_zone.py
@@ -9,9 +9,9 @@
 # 1. Set up the missing parameters in the parameter section
 # 2. Create an MQTT client using the paho-mqtt package with mqtt.Client()
 # 3. Define a callback function that will be executed when the client
-#    receives message on a subscribed topic_weather. It should decode your message
+#    receives message on a subscribed topic. It should decode your message
 #    and store the information for later in our history
-# 4. Subscribe to the topic_weather that the device will publish to
+# 4. Subscribe to the topic that the device will publish to
 # 5. Create a function that publishes the simulated temperature via MQTT as a JSON
 # 6. Run the simulation and plot
 
@@ -28,7 +28,7 @@ from urllib.parse import urlparse
 # ToDo: Enter your mqtt broker url and port, e.g mqtt://test.mosquitto.org:1883
 MQTT_BROKER_URL = "mqtt://test.mosquitto.org:1883"
 
-# ToDo: Create a topic_weather that your weather station will publish to
+# ToDo: Create a topic that your weather station will publish to
 topic_weather = "fiware_workshop/<name_surname>/weather_station"
 topic_room = "fiware_workshop/<name_surname>/room"
 
@@ -92,7 +92,7 @@ if __name__ == '__main__':
 
 
     # ToDo: Define a callback function that will be executed when the client
-    #  receives message on a subscribed topic_weather. It should decode your message
+    #  receives message on a subscribed topic. It should decode your message
     #  and store the information for later in our history
     #  Note: do not change function's signature
     def on_message(client, userdata, msg):
@@ -104,7 +104,7 @@ if __name__ == '__main__':
     # add your callback function to the client
     mqttc.on_message = on_message
 
-    # ToDO: connect to the mqtt broker and subscribe to your topic_weather
+    # ToDO: connect to the mqtt broker and subscribe to your topic
 
     # create a non-blocking thread for mqtt communication
     mqttc.loop_start()
