693bd3215318a7edf06f1f07f58d83d1e1da3163
JunsongDu <junsong.du@eonerc.rwth-aachen.de>
2023-12-07 11:30:57 +0100

    chore: adjust tutorials for authentication

--- examples/basics/e02_baerer_token.py
+++ examples/basics/e02_baerer_token.py
@@ -1,40 +1,31 @@
 """
-# Examples for initializing a contextbroker client with authorization via baerer token.
+# Examples for initializing a contextbroker client with authorization via bearer
+token.
 - The authorization key is provided via the FiwareHeaderSecure class.
 - The parameters should be provided via environment variables or a .env file.
 """
 
-from filip.models.base import FiwareHeader
+from filip.models.base import FiwareHeaderSecure
 from filip.clients.ngsi_v2 import ContextBrokerClient
-from pydantic import Field
- 
-class FiwareHeaderSecure(FiwareHeader):
-    """
-    Define service paths whith authorization
-    """
-    authorization: str = Field(
-        alias="authorization",
-        default="",
-        max_length=3000,
-        description="authorization key",
-        regex=r".*"
-    )
-    
 
 # ## Parameters
 # Host address of Context Broker
-CB_URL = "http://localhost:1026"
+CB_URL = "https://localhost:1026"
 # FIWARE-Service
 fiware_service = 'filip'
 # FIWARE-Servicepath
 fiware_service_path = '/example'
 # FIWARE-Bearer token
+# TODO it has to be replaced with the token of your protected endpoint
 fiware_baerer_token = 'BAERER_TOKEN'
-    
+
 if __name__ == '__main__':
-    fiware_header = FiwareHeaderSecure(service= fiware_service,
-                                       service_path= fiware_service_path,
-                                       authorization= f"""Bearer {fiware_baerer_token}""")
-    
-    cb_client = ContextBrokerClient(url = CB_URL,
+    fiware_header = FiwareHeaderSecure(service=fiware_service,
+                                       service_path=fiware_service_path,
+                                       authorization=f"""Bearer {
+                                       fiware_baerer_token}""")
+    cb_client = ContextBrokerClient(url=CB_URL,
                                     fiware_header=fiware_header)
+    # query entities from protected orion endpoint
+    entity_list = cb_client.get_entity_list()
+    print(entity_list)
