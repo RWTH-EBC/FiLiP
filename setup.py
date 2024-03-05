65c23f94568a889c2ca3b7e4c448c20fa75e77ce
JunsongDu <junsong.du@eonerc.rwth-aachen.de>
2024-03-05 16:41:44 +0100

    chore: change the python requirements in setup

--- setup.py
+++ setup.py
@@ -47,9 +47,10 @@ setuptools.setup(
     classifiers=['Development Status :: 3 - Alpha',
                  'Topic :: Scientific/Engineering',
                  'Intended Audience :: Science/Research',
-                 'Programming Language :: Python :: 3.7',
                  'Programming Language :: Python :: 3.8',
                  'Programming Language :: Python :: 3.9',
+                 'Programming Language :: Python :: 3.10',
+                 'Programming Language :: Python :: 3.11',
                  "License :: OSI Approved :: BSD License"],
     keywords=['iot', 'fiware', 'semantic'],
     packages=setuptools.find_packages(exclude=['tests',
@@ -66,6 +67,6 @@ setuptools.setup(
         ":python_version >= '3.9'": ["pandas~=2.1.4"]
     },
     install_requires=INSTALL_REQUIRES,
-    python_requires=">=3.7",
+    python_requires=">=3.8",
 
 )
