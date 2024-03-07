ce4d2e408979548b412507b9c21c680ec91e51f6
j.azzam <j.azzam@fz-juelich.de>
2024-03-07 14:34:15 +0100

    Adding more description into tutorial e8

--- tutorials/ngsi_v2/e8_multientity_and_expression_language/e8_multientity_and_expression_language_solution.py
+++ tutorials/ngsi_v2/e8_multientity_and_expression_language/e8_multientity_and_expression_language_solution.py
@@ -1,12 +1,23 @@
 """
 # # Exercise 8: MultiEntity and Expression Language
 
-#
+# The MultiEntity plugin Allows the devices provisioned in the IoTAgent to map their attributes to more than one entity,
+# declaring the target entity through the Configuration or Device provisioning APIs.
+
+# The IoTAgent Library provides an expression language for measurement transformation, that can be used to adapt the
+# information coming from the South Bound APIs to the information reported to the Context Broker. This is really useful
+# when you need to adapt measure.
+
+# There are available two different expression languages jexl and legacy. The recommended language to use is jexl,
+# which is newer and most powerful.
 
 # The input sections are marked with 'TODO'
 
 # #### Steps to complete:
-# 1.
+# 1. Setting up the expression language jexl
+# 2. Applying the expression language to device attributes
+# 3. Testing the expression language via MQTT messages
+# 4. Applying the expression language to device attributes in a multi-entity scenario
 """
 # Import packages
 import logging
