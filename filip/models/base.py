74cf079d6cadca8f64ddaf4148f92feb65ea9bfe
JunsongDu <junsong.du@eonerc.rwth-aachen.de>
2024-03-26 15:28:37 +0100

    chore: change default service_path to "/"

--- filip/models/base.py
+++ filip/models/base.py
@@ -103,7 +103,7 @@ class FiwareHeader(BaseModel):
     )
     service_path: str = Field(
         alias="fiware-servicepath",
-        default="",
+        default="/",
         description="Fiware service path",
         max_length=51,
     )
