a9d6910657735237a2dd0ed94bf5f9200c491282
JunsongDu <junsong.du@eonerc.rwth-aachen.de>
2025-02-12 16:31:33 +0100

    docs: update supported python version

--- setup.py
+++ setup.py
@@ -57,6 +57,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: BSD License",
     ],
     keywords=["iot", "fiware", "semantic"],
