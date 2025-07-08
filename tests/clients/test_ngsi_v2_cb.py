c9deee8eba1dc62e4c07fa22b9f570962fc35eb4
Junsong Du <junsong.du@eonerc.rwth-aachen.de>
2025-07-08 15:25:17 +0200

    chore: activate force update

--- tests/clients/test_ngsi_v2_cb.py
+++ tests/clients/test_ngsi_v2_cb.py
@@ -965,6 +965,7 @@ class TestContextBroker(unittest.TestCase):
             attr_name="temperature",
             value=new_value,
             entity_type=entity.type,
+            forcedUpdate=True,
         )
         # test if the subscriptions arrives and the content aligns with updates
         max_retry = 5
@@ -1413,7 +1414,10 @@ class TestContextBroker(unittest.TestCase):
             )
             time.sleep(2)
             client.update_attribute_value(
-                entity_id=entity.id, attr_name="temperature", value=10
+                entity_id=entity.id,
+                attr_name="temperature",
+                value=10,
+                forcedUpdate=True,
             )
             # check the notified entities
             max_retry = 5
